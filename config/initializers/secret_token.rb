207da08d2b6ca8a41dfc6fff32eed9736e9fb9e0
Rami Shomali <rami@mylookout.com>
2013-01-26 11:09:08 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Datagrubber::Application.config.secret_token = '6f4564908e4aac565b936ba10a073f8dcffff586c00f4812d697c5afb15de883a28f069eb9c0f10024f91eb69e0bdb007c8ead5f605349ceaec2a5be510f7e19'
