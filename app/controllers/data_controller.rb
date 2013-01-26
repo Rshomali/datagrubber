bd12d26c491b7a1256ab400d029a348651455242
Rami Shomali <rami@mylookout.com>
2013-01-26 13:55:54 +0000

    fix crime api

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -10,8 +10,12 @@ class DataController < ApplicationController
     #
     # get crime data
     #
-    response  = HTTParty.get('http://dia.offsetdesign.co.uk/api/crime/lat/long')
+    response  = HTTParty.get('http://dia.offsetdesign.co.uk/api/crime?lat=xxx&long=yyy')
     crime_data = JSON.parse(response.body)
+
+    #
+    # get 
+    #
 
     respond_to do |format|
       format.json { render :json => data.merge(crime_data) }
